26570bd12c447227fd18c442c1091af13e635a7c
HoangTung <tungvanzk03@gmail.com>
2023-11-16 09:24:51 +0700

    second

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
     lines: [
         "Luôn vui vẻ nhé!",
-        "Always have a smile on your face!",
+        "Always have a smile on your face 。。。",
     ],
     date: "2005-01-28T00:00:00",
     names: ["Lưu Quế Như",]
